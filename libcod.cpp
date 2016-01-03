cbb845aee2a714d017a1b292662a42deee6fee95
Alexey Ilin <alexey12424323@gmail.com>
2016-01-03 08:01:54 +0300

    Add gcc version string

--- libcod.cpp
+++ libcod.cpp
@@ -2565,7 +2565,7 @@ public:
         printf("> [WARNING] Compiled for: %s\n", TOSTRING1(COD_VERSION));
 #endif
 
-        printf("Compiled: " __DATE__ " " __TIME__ "\n");
+        printf("Compiled: " __DATE__ " " __TIME__ " using GCC " __VERSION__ "\n");
 
 
 #if COD_VERSION == COD4_1_7
